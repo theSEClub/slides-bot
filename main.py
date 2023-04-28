757e33b47aadc88704833b865996c6f2a8aa3fad
mohbisher <mohammed@mohbisher.com>
2023-04-28 19:32:17 +0300

    feat & refactor: enhanced response performance + restructured the logic into chunks

--- main.py
+++ main.py
@@ -1,99 +1,85 @@
 from datetime import datetime
+from typing import List, Dict, Any
 
 import cv2
 from cvzone.HandTrackingModule import HandDetector
-import Click
+from swipe_controller import SwipeController
+from time import sleep
 
-cap = cv2.VideoCapture(0)
+class Main:
+    def __init__(self):
+        os: str = input("What operating system are you using? (mac/windows): ").lower()
+        swipe_type: str = input("What do you want to swipe? (screens/slides): ").lower()
+        if os not in ("mac", "windows") or swipe_type not in ("screens", "slides"):
+            raise Exception("Invalid input")
+        print("Initializing software...")
 
-detector = HandDetector(detectionCon=0.5, maxHands=2)
-# color = (255, 0, 255)
-# cx, cy, w, h = 100, 100, 200, 200
-waitingSeconds = 1
+        self.cap: cv2.VideoCapture = cv2.VideoCapture(0)
+        self.detector: HandDetector = HandDetector(detectionCon=0.5, maxHands=2)
+        self.waiting_seconds: int = 1 * 1000
+        self.hand_types: Dict[str, Any] = {}
+        self.last_action: datetime = datetime.now()
+        self.check_right_counter: int = 0
+        self.check_left_counter: int = 0
+        self.last_check_right: datetime = datetime.now()
+        self.last_check_left: datetime = datetime.now()
+        self.swipe_controller: SwipeController = SwipeController(os, swipe_type)
 
-waitingSeconds = waitingSeconds * 1000
-handTypes = {}
-lastAction = datetime.now()
-checkRightCounter = 0
-checkLeftCounter = 0
-lastCheckRight = datetime.now()
-lastCheckLeft = datetime.now()
+    def run(self) -> None:
+        while True:
+            _, img = self.cap.read()
+            hands, img = self.detector.findHands(img)
+            self.process_hands(hands, img)
 
-print(lastAction)
+    def process_hands(self, hands: List[Any], img: cv2.Mat) -> None:
+        if hands:
+            self.process_single_hand(hands)
+            if len(hands) == 2:
+                self.process_two_hands(hands, img)
 
-while True:
-    # print('Reading')
-    _, img = cap.read()
-    hands, img = detector.findHands(img)
-    # hands, img = detector.findHands(img, flipType=False)
-    # hands = detector.findHands(img, draw=False)
+        img = cv2.flip(img, 1)
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
 
-    # cv2.rectangle(img, (cx-w//2, cy-h//2), (cx+w//2, cy+h//2), color, cv2.FILLED)
-
-    if hands:
+    def process_single_hand(self, hands: List[Any]) -> None:
         hand1 = hands[0]
-        lmList1 = hand1["lmList"]  # list of 21 landmarks points
-        bbox1 = hand1["bbox"]  # x, y, w, h
-        centerPoint1 = hand1["center"]  # cx, cy
-        handType1 = hand1["type"]
-        fingers1 = detector.fingersUp(hand1)
-        index_finger1 = lmList1[8]
-        middle_finger1 = lmList1[12]
+        hand_type1 = hand1["type"]
+        self.hand_types[hand_type1] = hand1
 
-        # cv2.circle(img, index_finger1, 10, color, cv2.FILLED)
-        handTypes[handType1] = hand1
+    def process_two_hands(self, hands: List[Any], img: cv2.Mat) -> None:
+        hand2 = hands[1]
+        hand_type2 = hand2["type"]
+        self.hand_types[hand_type2] = hand2
+        right_hand = self.hand_types.get("Right", 0)
+        left_hand = self.hand_types.get("Left", 0)
 
-        if len(hands) == 2:
-            hand2 = hands[1]
-            lmList2 = hand2["lmList"]  # list of 21 landmarks points
-            bbox2 = hand2["bbox"]  # x, y, w, h
-            centerPoint2 = hand2["center"]  # cx, cy
-            handType2 = hand2["type"]
-            fingers2 = detector.fingersUp(hand2)
-            handTypes[handType2] = hand2
-            rightHand = handTypes.get("Right", 0)
-            leftHand = handTypes.get("Left", 0)
-            if not rightHand or not leftHand:
-                continue
-            rightUpFingers = detector.fingersUp(rightHand)
-            leftUpFingers = detector.fingersUp(leftHand)
-            index_finger2 = lmList2[8]
-            middle_finger2 = lmList2[12]
-            if not leftUpFingers[0] and leftUpFingers[1] and leftUpFingers[2] and not leftUpFingers[3] and not leftUpFingers[4]:
-                index_finger1 = index_finger1[:2]
-                index_finger2 = index_finger2[:2]
-                length, info, img = detector.findDistance(index_finger1, index_finger2, img)
-                # length, info = detector.findDistance(index_finger1, index_finger2)
-                if length > 100:
-                    now = datetime.now()
-                    if (now - lastAction).seconds*1000 > waitingSeconds:
-                        lastAction = now
-                        print("Right")
-                        Click.swipe_right()
+        if not right_hand or not left_hand:
+            return None
 
-            if not rightUpFingers[0] and rightUpFingers[1] and rightUpFingers[2] and not rightUpFingers[3] and not \
-            rightUpFingers[4]:
-                index_finger1 = index_finger1[:2]
-                index_finger2 = index_finger2[:2]
-                length, info, img = detector.findDistance(index_finger1, index_finger2, img)
-                if length > 100:
-                    now = datetime.now()
-                    if (now - lastAction).seconds*1000 > waitingSeconds:
-                        checkLeftCounter += 1
-                        if (now - lastCheckLeft).seconds < 1:
-                            lastCheckLeft = now
-                            checkLeftCounter = 0
+        left_up_fingers = self.detector.fingersUp(left_hand)
+        right_up_fingers = self.detector.fingersUp(right_hand)
+        lm_list1 = left_hand["lmList"]
+        lm_list2 = right_hand["lmList"]
+        index_finger1 = lm_list1[8][:2]
+        index_finger2 = lm_list2[8][:2]
 
-                        if checkLeftCounter > 10:
-                            lastAction = now
-                            print("Left")
-                            Click.swipe_left()
-                            checkLeftCounter = 0
+        length, info, img = self.detector.findDistance(index_finger1, index_finger2, img)
+        self._set_last_action(length, index_finger1, index_finger2)
 
+        if self.swipe_controller.is_swipe_right(left_up_fingers):
+            self.swipe_controller.swipe_right()
+            sleep(0.5)
 
+        if self.swipe_controller.is_swipe_left(right_up_fingers):
+            self.swipe_controller.swipe_left()
+            sleep(0.5)
 
+    def _set_last_action(self, length: float, index_finger1: List[int], index_finger2: List[int]) -> None:
+        if length > 100:
+            now = datetime.now()
+            if (now - self.last_action).seconds * 1000 > self.waiting_seconds:
+                self.last_action = now
 
-    img = cv2.flip(img, 1)
-
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+if __name__ == "__main__":
+    main = Main()
+    main.run()
